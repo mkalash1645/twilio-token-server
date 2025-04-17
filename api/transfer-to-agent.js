3c9587235781c03d836c8b42253c5c7c58c785c8
mkalash1645 <mkalashian@kingsbarn.com>
2025-04-17 10:58:03 -0700

    Update transfer-to-agent.js

--- api/transfer-to-agent.js
+++ api/transfer-to-agent.js
@@ -17,27 +17,12 @@ export default async function handler(req, res) {
   }
 
   const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-  const conferenceName = 'KB_LiveSupport';
 
   try {
     const call = await client.calls.create({
       to: TARGET_NUMBER,
       from: TWILIO_NUMBER,
-twiml: `
-  <Response>
-    <Say>Connecting you to a potential investor who was speaking with our AI assistant.</Say>
-    <Dial>
-      <Conference 
-        startConferenceOnEnter="true" 
-        endConferenceOnExit="false" 
-        waitUrl="http://twimlets.com/holdmusic?Bucket=com.twilio.music.classical">
-        ${conferenceName}
-      </Conference>
-    </Dial>
-  </Response>
-`.trim()
-
-
+      url: "https://handler.twilio.com/twiml/EH0c65c99c31072a9ddf8245c6af471c54" // 👈 Your TwiML Bin URL
     });
 
     console.log('[TRANSFER] Call started:', call.sid);
