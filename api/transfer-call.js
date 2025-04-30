fd4a156b3d3490d0920d7b8a2899cb5e8fa7fd3c
mkalash1645 <mkalashian@kingsbarn.com>
2025-04-30 09:53:45 -0700

    Update transfer-call.js

--- api/transfer-call.js
+++ api/transfer-call.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   const { repName } = req.body;
 
   if (!TWILIO_ACCOUNT_SID || !TWILIO_AUTH_TOKEN || !TWILIO_NUMBER) {
-    return res.status(500).json({ message: 'Missing required environment variables' });
+    return res.status(500).json({ message: 'Missing environment variables' });
   }
 
   if (!repName) {
@@ -24,14 +24,14 @@ export default async function handler(req, res) {
   const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
   const repDirectory = {
-    "Anthony Goros": { number: "+14158799000", conference: "Conf_Anthony" },
-    "Jared Wolff": { number: "+19093409000", conference: "Conf_Jared" },
-    "Louie Goros": { number: "+14243439000", conference: "Conf_Louie" },
-    "Matt Ayer": { number: "+15624529000", conference: "Conf_Matt" },
-    "Alex Hardick": { number: "+18582409000", conference: "Conf_Alex" },
-    "Front Desk": { number: "+17026753263", conference: "Conf_Front" },
-    "Mason Kalashian": { number: "+17026753265", conference: "Conf_Mason" },
-    "Fallback": { number: "+19493019000", conference: "Conf_Fallback" }
+    "Anthony Goros": { number: "+14158799000", conference: "Conference_Anthony" },
+    "Jared Wolff": { number: "+19093409000", conference: "Conference_Jared" },
+    "Louie Goros": { number: "+14243439000", conference: "Conference_Louie" },
+    "Matt Ayer": { number: "+15624529000", conference: "Conference_Matt" },
+    "Alex Hardick": { number: "+18582409000", conference: "Conference_Alex" },
+    "Mason Kalashian": { number: "+17026753265", conference: "Conference_Mason" },
+    "Front Desk": { number: "+17026753263", conference: "Conference_Front" },
+    "Fallback": { number: "+19493019000", conference: "Conference_Fallback" }
   };
 
   const selectedRep = repDirectory[repName] || repDirectory["Fallback"];
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
           <Dial>
             <Conference 
               startConferenceOnEnter="true" 
-              endConferenceOnExit="false" 
+              endConferenceOnExit="false"
               waitUrl="http://twimlets.com/holdmusic?Bucket=com.twilio.music.classical">
               ${selectedRep.conference}
             </Conference>
@@ -55,7 +55,7 @@ export default async function handler(req, res) {
       `.trim(),
     });
 
-    console.log('[TRANSFER-CALL] Call initiated to:', repName);
+    console.log('[TRANSFER-CALL] Outbound call started:', call.sid);
     res.status(200).json({ message: 'Transfer call initiated', sid: call.sid });
   } catch (error) {
     console.error('[TRANSFER-CALL] Error:', error);
